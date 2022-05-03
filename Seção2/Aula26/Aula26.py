7aa7fbee01d9ffa4f27b545c389afcf69845dc9d
GTMacieira <macieira.gtalpo@gmail.com>
2022-05-02 22:01:02 -0300

    Aula_26_a_27

---
+++ Seção2/Aula26/Aula26.py
@@ -0,0 +1,9 @@
+"""
+Desempacotamento de lista
+"""
+
+lista= ['A', 'B', 'C',1,2,3,4,5,6,7]
+
+n1,n2,n3, *outra_lista = lista
+
+print(n1)
---
+++ Seção2/Aula27/Aula27.py
@@ -0,0 +1,11 @@
+"""
+Trocar valores de variáveis
+"""
+
+x=10
+y= 'Gui'
+z= "Macieira"
+
+x,y,z = z,x,y
+
+print(f'x={x}, y={y} e z={z}')
