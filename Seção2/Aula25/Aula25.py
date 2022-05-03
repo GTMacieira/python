dec1f3c0659ccc7216748f7a9befa7857c25c82f
GTMacieira <macieira.gtalpo@gmail.com>
2022-05-02 21:40:00 -0300

    Aula_1_a_25

--- Seção2/Aula25/Aula25.py
+++ Seção2/Aula25/Aula25.py
@@ -5,5 +5,48 @@ Split, Join e Enumerate em Python
 *Enumerate - Enumerar elementos da liosta #list
 """
 
+lista = [
+    [1,2],
+    [3,4],
+    [5,6]
+]
+
+n1,n2,n3 = lista
+
+print (n2)
+
+for v in lista:
+    print(v[0], v[1])
 
 
+
+# texto = "Você tem brio? Ou você tosquinho?"
+# lista_1= texto.split(' ')
+#
+# for indice, valor in enumerate(lista_1):
+#     print(indice, valor, lista_1[indice])
+
+
+
+
+# lista_2= texto.split('?')
+# texto_2 = ','.join(lista_1)
+
+# print(texto)
+# print(lista_1)
+# print(texto_2)
+
+
+# contagem = 0
+# palavra = ''
+
+# for valor in lista_2:
+#     print(valor.strip().capitalize())
+
+# for valor in lista_1:
+#     qtd_vezes = lista_1.count(valor)
+#     if qtd_vezes > contagem:
+#         contagem = qtd_vezes
+#         palavra = valor
+# print(f'A palavra que apareceu mais vezes é {palavra} ({contagem}x)')
+#     # print(f'A palavre {valor} apareceu {lista_1.count(valor)}x na frase')
