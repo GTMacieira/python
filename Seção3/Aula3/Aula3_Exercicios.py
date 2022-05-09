b8b4ea12d6f650681c2291b9cd94aec4aec6234f
GUILHERME TALPO MACIEIRA <guilherme.macieira.gm1@csn.com.br>
2022-05-09 16:53:01 -0300

    Exercios_propostos

---
+++ Seção3/Aula3/Aula3_Exercicios.py
@@ -0,0 +1,43 @@
+"""
+1 - Crie uma função que exibe uma saudaçõa com os parametros saudação e nome. 
+"""
+def saudacao(saud ='Olá', nome ='Usuário'):
+    return(saud, nome)
+
+var = saudacao('Bem vindo!', 'Guilherme')
+print(var)
+
+
+"""
+2 - Crie uma função que recebe 3 números como porâmetros e exibe a soma entre eles
+"""
+def soma(n1, n2):
+    return n1 + n2
+
+result = soma( 1, 7)
+print(result)
+
+"""
+3 - Crie uma função que 2 números. O prmeiro é o valor e o segundo um percentual (ex 10%). Retorne (return) o valor
+do primeiro número somado ao aumento do percentual do mesmo.
+"""
+
+def percent( int , per):
+    return int + int * per / 100
+
+result = percent ( 10, 10)
+print (result)
+
+"""
+4 - Fizz Buzz - Se o parâmetro da função for divisível por 2, retorne fizz, se o parâmetro da função for divisivel 
+por 5, retorne buzz. Se p parâmetro da função for divisivel por 5 e por 3, retorne FizzBuzz, caso contrário, retorne
+número enviado.
+"""""
+def FizzBuzz (val):
+    if val % 3 = 0 and:
+        return ('fizz')
+        
+
+    
+    
+    
