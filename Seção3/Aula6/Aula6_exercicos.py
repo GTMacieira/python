62835cc622c96016b82afa960b2ec084710527d8
GUILHERME TALPO MACIEIRA <guilherme.macieira.gm1@csn.com.br>
2022-05-17 14:03:21 -0300

    Exercicios

---
+++ Seção3/Aula6/Aula6_exercicos.py
@@ -0,0 +1,29 @@
+
+# 1- Crie uma função1 que recebe uma função2 como parâmetro e retorne o valor da função2 executada
+
+
+def func1(res):
+    print(res)
+        
+def func2():
+    res  = 'olá'
+    return res
+    
+var = func2()
+
+func1(var)
+
+    
+#2- Crie uma função1 que recebe uma função2 como parâmetro e retorne o valor da função2 executada. Faça a função1
+#executar duas funções que recebam um numero diferente de argumentos
+
+def func1(res):
+    print(res)
+        
+def func2():
+    res  = 'olá'
+    return res
+    
+var = func2()
+
+func1(var)
