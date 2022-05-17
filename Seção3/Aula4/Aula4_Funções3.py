7c0cddfe8c79ae8b111882c83c64963612019f76
GUILHERME TALPO MACIEIRA <guilherme.macieira.gm1@csn.com.br>
2022-05-17 11:29:05 -0300

    Args_Kwargs

--- Seção3/Aula4/Aula4_Funções3.py
+++ Seção3/Aula4/Aula4_Funções3.py
@@ -1,1 +1,22 @@
-def
+# def func(*args):
+#     print(args)
+#     print(args[0])
+#     print(args[-1])
+#     print(len(args))
+    
+# func(1,2,3,4,5)
+
+# def func(*args):
+#     args=list(args) #casting, caso contrario manteria-se uma tupla
+#     args[0]=20
+#     print(args)
+    
+# func(1,2,3,4,5)
+
+def func(*args, **kwargs): '''kwargs são argumentos com palavras chave argumento = "...",
+                             args não exibem kwargs e visse e versa'''
+    args=list(args) #casting, caso contrario manteria-se uma tupla
+    args[0]=20
+    print(args)
+    
+func(1,2,3,4,5)
